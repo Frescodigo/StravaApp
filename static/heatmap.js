2c6bbb8693f5174074b061019f6d99288ad1f147
Frescodigo <thescrambler140@gmail.com>
2024-08-13 21:08:00 -0400

    finally fixed the colors

--- static/heatmap.js
+++ static/heatmap.js
@@ -3,6 +3,22 @@ const tableBody = document.getElementById('table-body')
 
 const DAILY_MILES = new Array(371).fill(0.0);
 
+const today = new Date();
+const aYearAgo = new Date(
+  today.getFullYear() - 1,
+  today.getMonth(),
+  today.getDate()
+);
+const sundayAYearAgo = new Date(
+  aYearAgo.getFullYear(),
+  aYearAgo.getMonth(),
+  aYearAgo.getDate() - aYearAgo.getDay()
+);
+
+// this is gonna be used to determine which
+// color a cell gets in the heatmap
+let mostMilesRanInADay = -Infinity;
+
 async function getActivities(cutoff) {
   const cutoffInEpoch = Math.floor(cutoff.getTime() / 1000)
   const url = `${APP_URL}/activities?cutoff=${cutoffInEpoch}`
@@ -14,14 +30,39 @@ async function getActivities(cutoff) {
 
 function fillTable() {
   tableBody.innerHTML = '';
-  for (let i = 0; i < 7; i++) {
+
+  // this is used to calculate which color the cell
+  // should be in the heatmap
+  const fifthOfMax = mostMilesRanInADay / 5;
+
+  for (let day = 0; day < 7; day++) {
     const dataRow = document.createElement('tr');
-    for (let j = 0; j < 53; j++) {
+
+    // should show what day of the week it is =.=
+    const weekdayNames = ['', 'Mon', '', 'Wed', '', 'Fri', ''];
+    const dayLabelBox = document.createElement('td');
+    dayLabelBox.classList.add('day-label-box');
+    const dayLabel = document.createElement('span');
+    dayLabel.innerHTML = weekdayNames[day];
+    dayLabel.classList.add('day-label');
+    dayLabelBox.appendChild(dayLabel);
+    dataRow.appendChild(dayLabelBox);
+
+    for (let week = 0; week <= 52; week++) {
+
+      const miles = DAILY_MILES[7*week + day];
+
+      let heatmapLevel = Math.floor(miles / mostMilesRanInADay * 5);
+      if (heatmapLevel === 5) heatmapLevel = 4;
+
       const tableCell = document.createElement('td');
-      tableCell.classList.add('level-1');
+      tableCell.classList.add(`level-${heatmapLevel}`);
+
       const hiddenData = document.createElement('span');
       hiddenData.classList.add('sr-only');
-      hiddenData.innerHTML = DAILY_MILES[7*j + i];
+
+      hiddenData.innerHTML = miles;
+
       tableCell.appendChild(hiddenData);
       dataRow.appendChild(tableCell);
     }
@@ -31,20 +72,8 @@ function fillTable() {
 
 // shit is begging to be refactored
 async function updatePage() {
-  const today = new Date();
-  const aYearAgo = new Date(
-    today.getFullYear() - 1,
-    today.getMonth(),
-    today.getDate()
-  );
-  const sundayAYearAgo = new Date(
-    aYearAgo.getFullYear(),
-    aYearAgo.getMonth(),
-    aYearAgo.getDate() - aYearAgo.getDay()
-  );
   const activities = await getActivities(sundayAYearAgo);
 
-  let mostMilesRanInADay = 0;
   activities.forEach((activity) => {
     const activityDate = new Date(activity['start_date_local']);
     const timeDeltaInMS = activityDate - sundayAYearAgo;
